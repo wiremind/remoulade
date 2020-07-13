4724f17cfcaee49428364ed9bf112bc9ef2936e9
Daniel <dfsantosbu@unal.edu.co>
2020-07-13 11:01:46 +0200

    chore: add  example of pipelines usage.

---
+++ examples/pipeline/pipeline.py
@@ -0,0 +1,21 @@
+import remoulade
+
+from remoulade.brokers.rabbitmq import RabbitmqBroker
+from remoulade import pipeline
+
+broker = RabbitmqBroker()
+remoulade.set_broker(broker)
+
+
+@remoulade.actor
+def add(x, y=0):
+    print("sum({},{}) = {}".format(x, y, x + y))
+    return x + y
+
+
+broker.declare_actor(add)
+
+if __name__ == "__main__":
+    message = add.send(1, 2)
+    g = pipeline((add.message(i) for i in range(0, 11)))
+    g.run()
