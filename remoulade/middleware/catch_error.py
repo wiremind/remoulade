f99ad6203470604a35a75c3d59642f9120d33672
Jonathan Griffe <jonathan.griffe@gmail.com>
2021-08-18 10:18:15 +0200

    feat: add cleanup_actor option in pipeline composition

--- remoulade/middleware/catch_error.py
+++ remoulade/middleware/catch_error.py
@@ -17,7 +17,19 @@ class CatchError(Middleware):
 
     def after_process_message(self, broker, message, *, result=None, exception=None):
         if message.failed:
-            actor_name = self.get_option("cleanup_actor", broker=broker, message=message)
-            if actor_name:
-                actor = broker.get_actor(actor_name)
-                actor.send(message.asdict(), {"type": type(exception).__name__, "message": str(exception)})
+            cleanup_actor = self.get_option("cleanup_actor", broker=broker, message=message)
+            if cleanup_actor:
+                actor = broker.get_actor(cleanup_actor)
+                actor.send(message.actor_name, type(exception).__name__, message.args, message.kwargs)
+
+    def update_options_before_create_message(self, options, broker, actor_name):
+        from ..actor import Actor
+
+        cleanup_actor = options.get("cleanup_actor")
+        if isinstance(cleanup_actor, Actor):
+            options["cleanup_actor"] = cleanup_actor.actor_name
+
+        elif cleanup_actor is not None and not isinstance(cleanup_actor, str):
+            raise TypeError(f"cleanup_actor must be an Actor or a string, got {type(cleanup_actor)} instead")
+
+        return options
