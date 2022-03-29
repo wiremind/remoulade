f89f0836934fa1f4d41ecfbc4e801b4b36af4ed3
Cédric de Saint Martin <cdesaintmartin@wiremind.io>
2022-03-29 15:29:53 +0200

    fix(tests): test_rabbitmq_cli: use test rabbitmq_broker instead of real rabbitmq broker.

--- tests/benchmarks/test_rabbitmq_cli.py
+++ tests/benchmarks/test_rabbitmq_cli.py
@@ -5,10 +5,6 @@ import time
 import pytest
 
 import remoulade
-from remoulade.brokers.rabbitmq import RabbitmqBroker
-
-broker = RabbitmqBroker()
-remoulade.set_broker(broker)
 
 
 @remoulade.actor(queue_name="benchmark-throughput")
@@ -46,59 +42,41 @@ remoulade.declare_actors([throughput, fib, latency])
 
 @pytest.mark.skipif(os.getenv("CI") == "true", reason="test skipped on CI")
 @pytest.mark.benchmark(group="rabbitmq-100k-throughput")
-def test_rabbitmq_process_100k_messages_with_cli(benchmark, info_logging, start_cli):
+def test_rabbitmq_process_100k_messages_with_cli(benchmark, info_logging, start_cli, rabbitmq_broker):
     # Given that I've loaded 100k messages into RabbitMQ
     def setup():
-        # The connection error tests have the side-effect of
-        # disconnecting this broker so we need to force it to
-        # reconnect.
-        broker.clear_channel_pools()
-        del broker.connection
-
         for _ in range(100000):
             throughput.send()
 
         start_cli("tests.benchmarks.test_rabbitmq_cli")
 
     # I expect processing those messages with the CLI to be consistently fast
-    benchmark.pedantic(broker.join, args=(throughput.queue_name,), setup=setup)
+    benchmark.pedantic(rabbitmq_broker.join, args=(throughput.queue_name,), setup=setup)
 
 
 @pytest.mark.skipif(os.getenv("CI") == "true", reason="test skipped on CI")
 @pytest.mark.benchmark(group="rabbitmq-10k-fib")
-def test_rabbitmq_process_10k_fib_with_cli(benchmark, info_logging, start_cli):
+def test_rabbitmq_process_10k_fib_with_cli(benchmark, info_logging, start_cli, rabbitmq_broker):
     # Given that I've loaded 10k messages into RabbitMQ
     def setup():
-        # The connection error tests have the side-effect of
-        # disconnecting this broker so we need to force it to
-        # reconnect.
-        broker.clear_channel_pools()
-        del broker.connection
-
         for _ in range(10000):
             fib.send(random.choice([1, 512, 1024, 2048, 4096, 8192]))
 
         start_cli("tests.benchmarks.test_rabbitmq_cli")
 
     # I expect processing those messages with the CLI to be consistently fast
-    benchmark.pedantic(broker.join, args=(fib.queue_name,), setup=setup)
+    benchmark.pedantic(rabbitmq_broker.join, args=(fib.queue_name,), setup=setup)
 
 
 @pytest.mark.skipif(os.getenv("CI") == "true", reason="test skipped on CI")
 @pytest.mark.benchmark(group="rabbitmq-1k-latency")
-def test_rabbitmq_process_1k_latency_with_cli(benchmark, info_logging, start_cli):
+def test_rabbitmq_process_1k_latency_with_cli(benchmark, info_logging, start_cli, rabbitmq_broker):
     # Given that I've loaded 1k messages into RabbitMQ
     def setup():
-        # The connection error tests have the side-effect of
-        # disconnecting this broker so we need to force it to
-        # reconnect.
-        del broker.channel
-        del broker.connection
-
         for _ in range(1000):
             latency.send()
 
         start_cli("tests.benchmarks.test_rabbitmq_cli")
 
     # I expect processing those messages with the CLI to be consistently fast
-    benchmark.pedantic(broker.join, args=(latency.queue_name,), setup=setup)
+    benchmark.pedantic(rabbitmq_broker.join, args=(latency.queue_name,), setup=setup)
