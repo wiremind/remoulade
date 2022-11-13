10a26c5c4b6b6b4d1fe772a51877eab17cf2bde6
Mehdi Gmira <mgmira@wiremind.io>
2022-11-13 18:22:43 +0100

    format

--- examples/composition/setup.py
+++ examples/composition/setup.py
@@ -18,6 +18,6 @@ setup(
     entry_points={"console_scripts": [
         "composition_worker=composition.worker:run",
         "composition_ask_count_words=composition.count_words:ask_count_words",
-        "composition_serve=composition.serve:serve",
+        "composition_serve=composition.serve:serve"
     ]},
 )
