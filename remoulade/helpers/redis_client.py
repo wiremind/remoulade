dff74eed2222d4cf706a41d90f61e113d1d2d0fa
Mehdi Zeroual <mzeroual@wiremind.io>
2025-02-19 10:49:22 +0100

    fix(redis result backend): add async_get_result, add pytest-asyncio

--- remoulade/helpers/redis_client.py
+++ remoulade/helpers/redis_client.py
@@ -3,6 +3,7 @@ from typing import Optional
 from urllib.parse import urlparse
 
 import redis
+import redis.asyncio as redis_async
 
 
 def redis_client(url: Optional[str], **parameters):
@@ -20,3 +21,20 @@ def redis_client(url: Optional[str], **parameters):
         else:
             parameters["connection_pool"] = redis.ConnectionPool.from_url(url)
             return redis.Redis(**parameters)
+
+
+def async_redis_client(url: Optional[str], **parameters):
+    """"""
+    if url:
+        url_parsed = urlparse(url)
+        if url_parsed.scheme == "sentinel":
+            sentinel = redis_async.Sentinel(
+                [(url_parsed.hostname, url_parsed.port)],
+                sentinel_kwargs={"password": url_parsed.password},
+            )
+            return sentinel.master_for(
+                service_name=os.path.normpath(url_parsed.path).split("/")[1], password=url_parsed.password
+            )
+        else:
+            parameters["connection_pool"] = redis_async.ConnectionPool.from_url(url)
+            return redis_async.Redis(**parameters)
