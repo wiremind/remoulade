77ea64ca9af8c2c2d659ddfa39123bebb34b9209
Francky <fblondy@wiremind.io>
2025-10-03 09:17:53 +0200

    feat: heartbeat middleware

---
+++ remoulade/middleware/heartbeat.py
@@ -0,0 +1,79 @@
+# This file is a part of Remoulade.
+#
+# Copyright (C) 2017,2018 CLEARTYPE SRL <bogdan@cleartype.io>
+#
+# Remoulade is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# Remoulade is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import os
+import shutil
+import tempfile
+import threading
+import time
+from typing import Union
+
+from ..logging import get_logger
+from .middleware import Middleware
+
+
+class Heartbeat(Middleware):
+    """Make Remoulade's heart beats.
+
+    This middleware makes each worker thread writes a file
+    in the specified directory containing the timestamp they started their latest task.
+    You can specify a minimal interval in seconds (default to 1 minute) between each write.
+    The directory is created when starting if it does not exist
+    (default to your temporary directory+/remouladebeat).
+    """
+
+    def __init__(self, dir: Union[str, None] = None, interval: int = 60):
+        super().__init__()
+        self.basedir = dir
+        self.interval = interval
+        self.log = get_logger(__name__, "HeartbeatMiddleware")
+        # process-specific
+        self.subdirs: dict[int, str] = {}
+        # thread-specific
+        self.beats: dict[int, float] = {}
+        self.files: dict[int, str] = {}
+
+    def after_process_boot(self, broker):
+        if not self.basedir:
+            self.basedir = tempfile.gettempdir() + "remouladebeat"
+        os.makedirs(self.basedir, exist_ok=True)
+        self.log.debug("Created directory %s", self.basedir)
+
+    def before_worker_boot(self, broker, worker):
+        pid = os.getpid()
+        self.subdirs[pid] = tempfile.mkdtemp(dir=self.basedir, prefix=f"pid-{pid}-")
+        self.log.debug("Created directory %s", self.subdirs[pid])
+
+    def after_worker_thread_boot(self, broker, thread):
+        fd, self.files[thread.ident] = tempfile.mkstemp(dir=self.subdirs[os.getpid()], prefix=f"th-{thread.ident}-")
+        os.close(fd)
+
+    def before_process_message(self, broker, message):
+        ident = threading.get_ident()
+        if self.beats.get(ident, 0) + self.interval < (beat := time.monotonic()):
+            with open(self.files[ident], "w") as f:
+                f.write(f"{beat}")
+            self.beats[ident] = beat
+
+    def before_worker_thread_shutdown(self, broker, thread):
+        try:
+            os.remove(self.files[thread.ident])
+        except FileNotFoundError:
+            pass
+        del self.beats[thread.ident]
+
+    def before_worker_shutdown(self, broker, worker):
+        shutil.rmtree(self.subdirs[os.getpid()], ignore_errors=True)
---
+++ tests/middleware/test_heartbeat.py
@@ -0,0 +1,100 @@
+# This file is a part of Remoulade.
+#
+# Copyright (C) 2017,2018 WIREMIND SAS <dev@wiremind.fr>
+#
+# Remoulade is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# Remoulade is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import time
+from pathlib import Path
+
+from remoulade.brokers.stub import StubBroker
+from remoulade.middleware import Heartbeat
+from remoulade.worker import Worker
+
+
+def test_heartbeat(stub_broker: StubBroker, do_work, tmp_path: Path):
+    beatdir = tmp_path
+    stub_broker.add_middleware(Heartbeat(dir=str(beatdir), interval=1))
+    stub_broker.emit_after("process_boot")
+    worker = Worker(stub_broker, worker_timeout=100, worker_threads=1)
+    worker.start()
+
+    do_work.send()
+
+    stub_broker.join(do_work.queue_name)
+    worker.join()
+
+    assert beatdir.is_dir()
+    # one proc dir
+    assert len(list(beatdir.iterdir())) == 1
+    # one thread file
+    procdir = next(beatdir.iterdir())
+    assert len(list(procdir.iterdir())) == 1
+    beatfile = next(procdir.iterdir())
+    beat = float(beatfile.read_text())
+
+    do_work.send()
+    stub_broker.join(do_work.queue_name)
+    worker.join()
+    # This is flaky if it took more than 1 second to get here since the previous send
+    # But I guess there's bigger issues if it is the case.
+    assert float(beatfile.read_text()) == beat
+
+    time.sleep(1)
+    do_work.send()
+    stub_broker.join(do_work.queue_name)
+    worker.join()
+    assert float(beatfile.read_text()) > beat
+
+    worker.workers[0].stop()
+    worker.workers[0].join()
+    assert not beatfile.is_file()
+    worker.stop()
+    assert not procdir.is_dir()
+
+
+def test_multith_heartbeat(stub_broker: StubBroker, do_work, tmp_path: Path):
+    beatdir = tmp_path
+    stub_broker.add_middleware(Heartbeat(dir=str(beatdir), interval=1))
+    stub_broker.emit_after("process_boot")
+    worker = Worker(stub_broker, worker_timeout=100, worker_threads=2)
+    worker.start()
+    t0 = worker.workers[0]
+    t1 = worker.workers[1]
+    t0.pause()
+    t0.paused_event.wait()
+    do_work.send()
+    stub_broker.join(do_work.queue_name)
+    worker.join()
+
+    t1.pause()
+    t1.paused_event.wait()
+    t0.resume()
+    do_work.send()
+    stub_broker.join(do_work.queue_name)
+    worker.join()
+
+    assert beatdir.is_dir()
+    # one proc dir
+    assert len(list(beatdir.iterdir())) == 1
+    # two thread file
+    procdir = next(beatdir.iterdir())
+    assert len(list(procdir.iterdir())) == 2
+    assert all(float(f.read_text()) for f in procdir.iterdir())
+
+    t0.stop()
+    t0.join()
+    assert not any(str(t0.ident) in str(p) for p in procdir.iterdir())
+    assert len(list(procdir.iterdir())) == 1
+    worker.stop()
+    assert not procdir.is_dir()
