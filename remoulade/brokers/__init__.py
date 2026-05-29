f48ad0b8cb8a715b0df56a347d264d0710347ffa
mducros <mducros@wiremind.io>
2026-05-29 10:57:09 +0200

    fix(app): works without pgmq extra

--- remoulade/brokers/__init__.py
+++ remoulade/brokers/__init__.py
@@ -14,10 +14,3 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-from .local import LocalBroker
-from .pgmq import PgmqBroker
-from .rabbitmq import RabbitmqBroker
-from .stub import StubBroker
-
-__all__ = ["LocalBroker", "PgmqBroker", "RabbitmqBroker", "StubBroker"]
